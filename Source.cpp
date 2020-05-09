b22c6cefc5414c131ce9905f42e8cd3237f2ae1a
ZirvedaAytimur <zirvedaytimur@gmail.com>
2020-05-09 15:39:25 +0300

    The project started

---
+++ Source.cpp
@@ -0,0 +1,37 @@
+﻿#include<stdio.h>
+#include<allegro5/allegro.h>
+#include<allegro5/allegro_primitives.h>
+
+int main() {
+
+	al_init_primitives_addon();
+	ALLEGRO_DISPLAY* display = NULL;
+	ALLEGRO_EVENT_QUEUE* queue = NULL;
+	ALLEGRO_TIMER* timer;
+
+	al_init();
+	al_install_keyboard();
+
+	display = al_create_display(600, 600);
+	timer = al_create_timer(1.0 / 20); // saniyede gönderme hızı
+	queue = al_create_event_queue();
+	al_start_timer(timer);
+
+	al_register_event_source(queue, al_get_display_event_source(display));
+	al_register_event_source(queue, al_get_keyboard_event_source());
+	al_register_event_source(queue, al_get_timer_event_source(timer));
+
+	while (1)
+	{
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(queue, &ev);
+
+		al_clear_to_color(al_map_rgb(0, 0, 0));
+	}
+
+	system("pause");
+	al_destroy_display(display);
+	al_destroy_event_queue(queue);
+
+	return 0;
+}
