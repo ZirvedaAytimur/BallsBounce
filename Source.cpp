3e843620ca11b2e24a9a14ca5c07c3e11d0aeb22
ZirvedaAytimur <zirvedaytimur@gmail.com>
2020-05-16 18:12:32 +0300

    Box added and some colors changed.

--- Source.cpp
+++ Source.cpp
@@ -27,6 +27,40 @@ int main() {
 	int layerXPos = 280, layerYPos = 650; // for layer
 	int ballXPos = 310, ballYPos = 640; // for ball
 	int direction = 1; // for ball's move
+	int boxFirstLineX = 0, boxFirstLineY = 50; // first line of boxes begin and end points
+	int boxSecondLineX = 0, boxSecondLineY = 111; // second of line of boxes begin and end points
+	int exists = 1; // if exists is equal to 1 the ball didn't hit yet else it is equal to 0
+	int numberOfBalls = 20;
+
+	int boxes[20][3];
+
+	// defining first box at the first line
+	boxes[0][0] = boxFirstLineX;
+	boxes[0][1] = boxFirstLineY;
+	boxes[0][2] = exists;
+
+	// defining other boxes at the first line
+	for (int i = 1; i < 10; i++)
+	{
+		boxFirstLineX += 61;
+		boxes[i][0] = boxFirstLineX;
+		boxes[i][1] = boxFirstLineY;
+		boxes[i][2] = exists;
+	}
+
+	// defining first box at the second line
+	boxes[10][0] = boxSecondLineX;
+	boxes[10][1] = boxSecondLineY;
+	boxes[10][2] = exists;
+
+	// defining other boxes at the second line
+	for (int i = 11; i < 20; i++)
+	{
+		boxSecondLineX += 61;
+		boxes[i][0] = boxSecondLineX;
+		boxes[i][1] = boxSecondLineY;
+		boxes[i][2] = exists;
+	}
 
 	al_register_event_source(queue, al_get_display_event_source(display));
 	al_register_event_source(queue, al_get_keyboard_event_source());
@@ -41,9 +75,9 @@ int main() {
 
 		// text
 
-		al_draw_text(font, al_map_rgb(255, 0, 255), 20, 10, 0, "Last");
-		al_draw_textf(font, al_map_rgb(255, 0, 255), 70, 10, 0, "%d",layerXPos);
-		al_draw_text(font, al_map_rgb(255, 0, 255), 120, 10, 0, "Balls");
+		al_draw_text(font, al_map_rgb(222, 15, 306), 20, 10, 0, "Last");
+		al_draw_textf(font, al_map_rgb(222, 15, 306), 70, 10, 0, "%d", numberOfBalls);
+		al_draw_text(font, al_map_rgb(222, 15, 306), 120, 10, 0, "Balls");
 
 		// layer
 
@@ -51,48 +85,48 @@ int main() {
 		{
 			layerXPos += 10;
 		}
-		else if (ev.keyboard.keycode==ALLEGRO_KEY_LEFT && layerXPos>20)
+		else if (ev.keyboard.keycode == ALLEGRO_KEY_LEFT && layerXPos > 20)
 		{
 			layerXPos -= 10;
 		}
 
 		// ball's direction
 
-		if (abs(ballYPos-layerYPos)<=10)
+		if (abs(ballYPos - layerYPos) <= 10)
 		{
 			direction = 1;
 		}
-		else if (abs(ballXPos-600)<=10)
+		else if (abs(ballXPos - 600) <= 10)
 		{
 			direction = 2;
 		}
-		else if (ballYPos>=50&&ballYPos<=60)
+		else if (ballYPos >= 50 && ballYPos <= 60)
 		{
 			direction = 3;
 		}
-		else if (ballXPos<=10)
+		else if (ballXPos <= 10)
 		{
 			direction = 4;
 		}
 
 		// balls's movement
 
-		if (direction==1)
+		if (direction == 1)
 		{
 			ballXPos += 3;
 			ballYPos -= 3;
 		}
-		else if (direction==2)
+		else if (direction == 2)
 		{
 			ballXPos -= 3;
 			ballYPos -= 3;
 		}
-		else if (direction==3)
+		else if (direction == 3)
 		{
 			ballXPos -= 3;
 			ballYPos += 3;
 		}
-		else if(direction==4)
+		else if (direction == 4)
 		{
 			ballXPos += 3;
 			ballYPos += 3;
@@ -100,13 +134,22 @@ int main() {
 
 		// end of the game
 
-		if ((ballXPos<layerXPos||ballXPos>layerXPos+60)&&ballYPos>=640)
+		if ((ballXPos<layerXPos || ballXPos>layerXPos + 60) && ballYPos >= 640)
 		{
 			system("pause");
 		}
 
-		al_draw_filled_rectangle(layerXPos, layerYPos, layerXPos + 60, layerYPos + 10, al_map_rgb(255, 255, 255));
-		al_draw_filled_circle(ballXPos, ballYPos, 10, al_map_rgb(255, 255, 255));
+		al_draw_filled_rectangle(layerXPos, layerYPos, layerXPos + 60, layerYPos + 10, al_map_rgb(246, 107, 107));
+		al_draw_filled_circle(ballXPos, ballYPos, 10, al_map_rgb(240, 204, 204));
+
+		for (int i = 0; i < 20; i++)
+		{
+			if (boxes[i][2] == 1)
+			{
+				al_draw_filled_rectangle(boxes[i][0], boxes[i][1], boxes[i][0] + 60, boxes[i][1] + 60, al_map_rgb(255, 255, 255));
+			}
+		}
+		
 		al_flip_display();
 	}
 
